b84a71baf41298e272d0eec42b63c5eb183e99d5
Giovanni Guarino <gioguarino@gmail.com>
2024-12-06 00:13:23 +0100

    chore: initial setup

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // 1 semana
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
